37c0e42df3e6a1972dd43ec7e6c68a084f4ef315
Matthew Martin <zhintateo19924559@gmail.com>
2020-08-30 14:18:58 -0500

    Updates

---
+++ precache-manifest.d3cfbc939a79227aabae09ee5ea3ff2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673c95837b69cb29e7d3b501e21afe53",
+    "url": "/front-end/index.html"
+  },
+  {
+    "revision": "7ea7aad03af28a63d7a3",
+    "url": "/front-end/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "443ec5b73326e26c8e88",
+    "url": "/front-end/static/css/main.a342ab8c.chunk.css"
+  },
+  {
+    "revision": "7ea7aad03af28a63d7a3",
+    "url": "/front-end/static/js/2.ac247f74.chunk.js"
+  },
+  {
+    "revision": "f6a33b08b8dab27dd8ab525dc9994832",
+    "url": "/front-end/static/js/2.ac247f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443ec5b73326e26c8e88",
+    "url": "/front-end/static/js/main.79ec14ef.chunk.js"
+  },
+  {
+    "revision": "7cb872770dd41e4c9cba",
+    "url": "/front-end/static/js/runtime-main.ff5dd159.js"
+  }
+]);
